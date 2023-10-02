b55ea6b851a4b95b5fa2567f0e8d8199a46526f4
Noitty <joan.ruizdeazua@i2cat.net>
2023-10-02 10:30:27 +0200

    Included the install script to install all the dependencies
    Signed-off-by: Noitty <joan.ruizdeazua@i2cat.net>

---
+++ install.sh
@@ -0,0 +1,26 @@
+sudo apt update
+
+sudo apt install qemu-system libvirt-daemon-system
+
+sudo apt install virt-manager
+
+sudo apt install pip
+
+pip install skyfield
+
+pip install toml
+
+pip install czml
+
+pip install flask
+
+pip install julian
+
+pip install astropy
+
+sudo apt install bridge-utils
+
+sudo apt install sshpass
+
+virsh --connect=qemu:///system net-start default
+
